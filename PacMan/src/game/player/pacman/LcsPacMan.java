740ae38c79b578a7481c747e974f82ea0cf706aa
alex <alex@lanin.de>
2012-01-10 19:06:50 +0000

    MoveAction

--- PacMan/src/game/player/pacman/LcsPacMan.java
+++ PacMan/src/game/player/pacman/LcsPacMan.java
@@ -3,7 +3,8 @@ package game.player.pacman;
 import game.core.G;
 import game.core.Game;
 import game.player.pacman.lcs.DistanceCondition;
-import game.player.pacman.lcs.MultipleConditions;
+import game.player.pacman.lcs.MoveAction;
+import game.player.pacman.lcs.Rule;
 import game.player.pacman.lcs.RuleFunctions;
 import game.player.pacman.lcs.Thing;
 import gui.AbstractPlayer;
@@ -17,12 +18,18 @@ public final class LcsPacMan extends AbstractPlayer{
 	
 	@Override
 	public int getAction(Game game,long timeDue){
-		Vector<MultipleConditions> conditions = new Vector<MultipleConditions>();
+		Vector<Rule> conditions = new Vector<Rule>();
 		
-		MultipleConditions test = new MultipleConditions();
+		Rule test = new Rule();
 		test.add(new DistanceCondition(Thing.GHOST, 5, 10));
 		test.add(new DistanceCondition(Thing.PILL, 1, 3));
+		test.setAction(new MoveAction(Thing.GHOST, true));
 		conditions.add(test);
+		
+		// dann nachm motto:
+		if(test.match(game)) test.getActionDirection(game);
+		
+		
 		
 		this.game = game;
 
