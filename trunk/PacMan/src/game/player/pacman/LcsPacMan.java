f713e7c75f2a181f93be893b50943acace2ff77a
alex@lanin.de <alex@lanin.de@1dcf074c-340a-367e-bcdd-14ebffc5a148>
2012-01-10 17:15:03 +0000

    SetOfPaths anfang

--- trunk/PacMan/src/game/player/pacman/LcsPacMan.java
+++ trunk/PacMan/src/game/player/pacman/LcsPacMan.java
@@ -1,28 +1,67 @@
 package game.player.pacman;
 
-import java.awt.Color;
-
 import game.core.G;
 import game.core.Game;
 import game.core.GameView;
 import gui.AbstractPlayer;
 
-public final class LcsPacMan extends AbstractPlayer{	
+import java.awt.Color;
+import java.util.Vector;
+
+public final class LcsPacMan extends AbstractPlayer{
+	class SetOfPaths {
+		private Vector<int[]> paths = new Vector<int[]>();
+		
+		void add(int[] path) {
+			paths.add(path);
+		}
+		
+		int[] getShortest() {
+			if(paths.size() == 0) throw new NullPointerException("Ej du Opfer, es sind keine Pfade im Set!");
+			
+			int[] shortest = null;
+			
+			for (int[] path : paths) {
+				if(shortest == null || shortest.length > path.length)
+					shortest = path;
+			}
+			
+			assert shortest != null;
+			return shortest;
+		}
+
+		public boolean isEmpty() {
+			return paths.isEmpty();
+		}
+	}
+
 	@Override
 	public int getAction(Game game,long timeDue){
 		int currentLocation = game.getCurPacManLoc();
+
+		int[] activePills = game.getPillIndicesActive();
+		
+		SetOfPaths pathsToGhosts = new SetOfPaths();
+		
+		for(int i=0;i<G.NUM_GHOSTS;i++) {
+			if(game.getLairTime(i) > 0) continue; // ignore erstmal
+
+			pathsToGhosts.add(game.getPath(currentLocation, game.getCurGhostLoc(i)));
+		}
+		
+		if(!pathsToGhosts.isEmpty()) {
+			int[] nextGhost = pathsToGhosts.getShortest();
+		
+			GameView.addPoints(game,Color.RED, nextGhost);
+		}
 		
 		
+		int[] directions=game.getPossiblePacManDirs(false);
+		int[] neighbours = game.getPacManNeighbours();
 		
-		for(int i=0;i<G.NUM_GHOSTS;i++)
-			if(game.getLairTime(i)==0)
-				if(game.isEdible(i))
-					GameView.addPoints(game,Color.GREEN,game.getPath(currentLocation,game.getCurGhostLoc(i)));
-				else
-					GameView.addPoints(game,Color.RED,game.getPath(currentLocation,game.getCurGhostLoc(i)));
-		
-		
-		int[] directions=game.getPossiblePacManDirs(false);		//set flag as false to prevent reversals	
+		if(!pathsToGhosts.isEmpty()) {
+			// TODO wenn neighbor[i] == ghostPfad[1] dann diesen weg abwerten oder so
+		}
 		return directions[G.rnd.nextInt(directions.length)];		
 	}
 
