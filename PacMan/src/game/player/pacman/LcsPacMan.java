2516e905148a1e88eb31899b5b8d86f8370f0ac1
alex <alex@lanin.de>
2012-01-10 16:16:21 +0000

    pfad zu geistern zeichnen - beispiel

--- PacMan/src/game/player/pacman/LcsPacMan.java
+++ PacMan/src/game/player/pacman/LcsPacMan.java
@@ -1,12 +1,27 @@
 package game.player.pacman;
 
+import java.awt.Color;
+
 import game.core.G;
 import game.core.Game;
+import game.core.GameView;
 import gui.AbstractPlayer;
 
 public final class LcsPacMan extends AbstractPlayer{	
 	@Override
-	public int getAction(Game game,long timeDue){			
+	public int getAction(Game game,long timeDue){
+		int currentLocation = game.getCurPacManLoc();
+		
+		
+		
+		for(int i=0;i<G.NUM_GHOSTS;i++)										
+			if(game.getLairTime(i)==0)
+				if(game.isEdible(i))
+					GameView.addPoints(game,Color.GREEN,game.getPath(currentLocation,game.getCurGhostLoc(i)));
+				else
+					GameView.addPoints(game,Color.RED,game.getPath(currentLocation,game.getCurGhostLoc(i)));
+		
+		
 		int[] directions=game.getPossiblePacManDirs(false);		//set flag as false to prevent reversals	
 		return directions[G.rnd.nextInt(directions.length)];		
 	}
