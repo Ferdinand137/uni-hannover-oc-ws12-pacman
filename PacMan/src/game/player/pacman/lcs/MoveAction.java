10261d645bc53355c34856014658c5329c4c54b8
alex <alex@lanin.de>
2012-01-11 08:01:04 +0000

    MoveAction.getDirection erstellt, ungetestet

--- PacMan/src/game/player/pacman/lcs/MoveAction.java
+++ PacMan/src/game/player/pacman/lcs/MoveAction.java
@@ -22,7 +22,25 @@ public class MoveAction {
 
 
 	int getDirection(Game game) {
-		// FIXME
-		return Game.UP;
+		int direction;
+		
+		switch (thing) {
+		case GHOST:
+			direction = RuleFunctions.getNextGhostDirection();
+			break;
+		case PILL:
+			direction = RuleFunctions.getNextPillDirection();
+			break;
+		case POWER_PILL:
+			direction = RuleFunctions.getNextPowerPillDirection();
+			break;
+		default:
+			throw new RuntimeException("this never ever happens... but there is an uninitialized direction error without this");
+		}
+		
+		if(moveAway)
+			direction = game.getReverse(direction);
+		
+		return direction;
 	}
 }
