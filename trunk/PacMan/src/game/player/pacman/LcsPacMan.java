f9827a2320a34e888d1d196c3db73d034166b71e
c.protsch@gmail.com <c.protsch@gmail.com@1dcf074c-340a-367e-bcdd-14ebffc5a148>
2012-01-12 10:01:44 +0000

    - einfache Auswahl der nächsten Richtung - Initialisierung der meisten Regeln - FIXME: Methoden aus anderen Klassen liefern ungültige Werte

--- trunk/PacMan/src/game/player/pacman/LcsPacMan.java
+++ trunk/PacMan/src/game/player/pacman/LcsPacMan.java
@@ -1,8 +1,9 @@
 package game.player.pacman;
 
-import game.core.G;
 import game.core.Game;
+import game.player.pacman.lcs.BlinkingCondition;
 import game.player.pacman.lcs.DistanceCondition;
+import game.player.pacman.lcs.EdibleCondition;
 import game.player.pacman.lcs.MoveAction;
 import game.player.pacman.lcs.Rule;
 import game.player.pacman.lcs.RuleFunctions;
@@ -26,23 +27,103 @@ public final class LcsPacMan extends AbstractPlayer{
 		
 		Vector<Rule> conditions = new Vector<Rule>();
 		
-		Rule test = new Rule();
-		test.add(new DistanceCondition(Thing.GHOST, 5, 10));
-		test.add(new DistanceCondition(Thing.PILL, 1, 3));
-		test.setAction(new MoveAction(Thing.GHOST, true));
-		RuleFunctions.getNextPillDistance();
-		RuleFunctions.getNextPowerPillDistance();
-		conditions.add(test);
+//		Rule test = new Rule();
+//		test.add(new DistanceCondition(Thing.GHOST, 5, 10));
+//		test.add(new DistanceCondition(Thing.PILL, 1, 3));
+//		test.setAction(new MoveAction(Thing.GHOST, true));
+//		RuleFunctions.getNextPillDistance();
+//		RuleFunctions.getNextPowerPillDistance();
+//		conditions.add(test);
+
+		Rule rule1 = new Rule();
+		rule1.add(new DistanceCondition(Thing.GHOST, 3.75f, 5));
+		rule1.add(new DistanceCondition(Thing.POWER_PILL, 2.5f, 7.5f));
+		rule1.add(new EdibleCondition(false));
+		rule1.setAction(new MoveAction(Thing.POWER_PILL, true));
+		conditions.add(rule1);
+
+		// TODO: was ist der Unterschied zwischen "not edible" und "ungleich edible"??? 
+//		Rule rule2 = new Rule();
+//		rule2.add(new DistanceCondition(Thing.GHOST, 0, 2.5f));
+//		rule2.add(new DistanceCondition(Thing.POWER_PILL, 0, 10));
+//		rule2.add(new EdibleCondition(false));
+//		rule2.setAction(new MoveAction(Thing.POWER_PILL, true));
+//		conditions.add(rule2);
+
+		Rule rule3 = new Rule();
+		rule3.add(new DistanceCondition(Thing.GHOST, 38.671875f, 40.390625f));
+		rule3.add(new EdibleCondition(false));
+		rule3.setAction(new MoveAction(Thing.GHOST, false));
+		conditions.add(rule3);
+
+		Rule rule4 = new Rule();
+		rule4.add(new DistanceCondition(Thing.GHOST, 5, 10));
+		rule4.add(new DistanceCondition(Thing.POWER_PILL, 5, 10));
+		rule4.setAction(new MoveAction(Thing.PILL, true));
+		conditions.add(rule4);
+
+		Rule rule5 = new Rule();
+		rule5.add(new DistanceCondition(Thing.PILL, 0, 55));
+		rule5.setAction(new MoveAction(Thing.PILL, true));
+		conditions.add(rule5);
+
+		Rule rule6 = new Rule();
+		rule6.add(new DistanceCondition(Thing.GHOST, 0, 13.75f));
+		rule6.add(new BlinkingCondition(false));
+		rule6.add(new EdibleCondition(false));
+		rule6.setAction(new MoveAction(Thing.GHOST, false));
+		conditions.add(rule6);
+
+		// TODO: junction ...
+		//Rule rule7 = new Rule();
+		
+		Rule rule8 = new Rule();
+		rule8.add(new DistanceCondition(Thing.POWER_PILL, 0, 27.5f));
+		rule8.setAction(new MoveAction(Thing.POWER_PILL, false));
+		conditions.add(rule8);
+		
+		// #UP=0, #RIGHT=1, #DOWN=2, #LEFT=3, #gesamt=4 
+		int[] direction_counter = new int[5];
+		float[] direction_weight = new float[4];
+		
+		for (Rule rule : conditions) {
+			System.out.println(rule.match(game));
+			if (rule.match(game)) {
+				// FIXME: game.player.pacman.lcs.RuleFunctions.getNextPillDirection wirft ArrayIndexOutOfBoundsException:
+				int dir = rule.getActionDirection(game);
+				// FIXME: getActionDirection liefert immer 4
+				// sollte ja eigentlich immer zwichen 0 und 3 liefern
+				// oder habe ich da was falsch verstanden?
+				System.out.println("getActionDirection: " + dir);
+				direction_counter[dir]++;
+				direction_counter[4]++;
+			}
+		}
+		
+		int dir = -1;
+		for (int i = 0; i < 4; i++) {
+			float max_weight = 0;
+			if (direction_counter[4] > 0) {
+				direction_weight[i] = ((float)direction_counter[i])/direction_counter[4];
+				// TODO: Entscheidungsregel, wenn zwei oder mehr Richtungen gleichhaeufig vorkommen
+				if (direction_weight[i] > max_weight) {
+					dir = i;
+					max_weight = direction_weight[i];
+				}
+			}
+		}
+		
 		
 		// dann nachm motto:
-		if(test.match(game)) test.getActionDirection(game);
+		//if(test.match(game)) test.getActionDirection(game);
 		// TODO aus conditions alle raussuchen wo match true
 		// von denen raussuchen in welche richtung sie laufen wollen
 		// erstmal zB einfache mehrheitentscheidung
-		test.getActionDirection(game); // sollte funktionieren sobald marcus seins fertig hat
+		//test.getActionDirection(game); // sollte funktionieren sobald marcus seins fertig hat
 		
 		int[] directions=game.getPossiblePacManDirs(false);
-		return directions[G.rnd.nextInt(directions.length)];		
+		//return directions[G.rnd.nextInt(directions.length)];		
+		return dir;
 	}
 
 	@Override
