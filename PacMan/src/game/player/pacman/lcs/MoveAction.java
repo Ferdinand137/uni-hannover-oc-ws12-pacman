9b74544bca5348b844f882e1a1fb21c59dbfd072
alex <alex@lanin.de>
2012-01-12 14:41:53 +0000

    new:  MoveRecommendation, 0 plan obs geht!

--- PacMan/src/game/player/pacman/lcs/MoveAction.java
+++ PacMan/src/game/player/pacman/lcs/MoveAction.java
@@ -21,7 +21,7 @@ public class MoveAction {
 	}
 
 
-	int getDirection(Game game) {
+	MoveRecommendation getDirection(Game game) {
 		int direction;
 		
 		switch (thing) {
@@ -38,9 +38,16 @@ public class MoveAction {
 			throw new RuntimeException("this never ever happens... but there is an uninitialized direction error without this");
 		}
 		
-		if(moveAway)
+		/*
+		if(moveAway) {
 			direction = game.getReverse(direction);
 		
-		return direction;
+			if(game.getNeighbour(game.getCurPacManDir(), direction) == -1) {
+				// so n pech aber auch, in die richtung gehts nicht weiter!
+			}
+		}
+		*/
+		
+		return new MoveRecommendation(direction, moveAway ? -1 : 1);
 	}
 }
