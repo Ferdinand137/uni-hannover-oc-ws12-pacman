1f27240225ec079636c92a286351aaf60df10d7e
c.protsch <c.protsch@gmail.com>
2012-01-12 15:49:35 +0000

    leere JunctionCondition

---
+++ PacMan/src/game/player/pacman/lcs/JunctionCondition.java
@@ -0,0 +1,13 @@
+package game.player.pacman.lcs;
+
+import game.core.Game;
+
+public class JunctionCondition implements Condition {
+
+	@Override
+	public boolean match(Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
