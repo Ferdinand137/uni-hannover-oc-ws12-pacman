c1d71b68378a9c433748253328440bb0ffbb8fb5
alex@lanin.de <alex@lanin.de@1dcf074c-340a-367e-bcdd-14ebffc5a148>
2012-01-13 16:31:29 +0000

    new enum: Direction. Für lesbarkeit und um die schleifen bis 4 in foreach schleifen umzuwandeln

---
+++ trunk/PacMan/src/game/player/pacman/lcs/Direction.java
@@ -0,0 +1,25 @@
+package game.player.pacman.lcs;
+
+public enum Direction {
+	UP, RIGHT, LEFT, DOWN;
+	
+	public static Direction createFromInt(int dir) {
+		switch (dir) {
+		case 0:  return UP;
+		case 1:  return RIGHT;
+		case 2:  return DOWN;
+		case 3:  return LEFT;
+		default: throw new RuntimeException("ungültige Richtung");
+		}
+	}
+	
+	public int toInt() {
+		switch(this) {
+		case UP:    return 0;
+		case RIGHT: return 1;
+		case DOWN:  return 2;
+		case LEFT:  return 3;
+		default:	throw new RuntimeException("ungültige Richtung");
+		}
+	}
+}
