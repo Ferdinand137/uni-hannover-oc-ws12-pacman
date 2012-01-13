ad6f395f5b8209d8b8872be5a8d9dc30795dda85
alex <alex@lanin.de>
2012-01-13 11:51:58 +0000

    junction versuche... fail

--- PacMan/src/game/player/pacman/lcs/MoveAction.java
+++ PacMan/src/game/player/pacman/lcs/MoveAction.java
@@ -1,5 +1,6 @@
 package game.player.pacman.lcs;
 
+import game.core.G;
 import game.core.Game;
 import game.core.GameView;
 
@@ -39,6 +40,55 @@ public class MoveAction {
 			return moveRecommendation;
 			
 		case JUNCTION:
+			for(int startDirection = 0; startDirection < 4; startDirection++) {
+				
+				int pos = game.getPacManNeighbours()[startDirection];
+				// nicht gegen die Wand planen!
+				if(pos == -1) {
+					System.out.println("wand: " + startDirection);
+					continue;
+				}
+				
+				int currentDirection = startDirection;
+
+				while(true) {
+					int newPos = game.getNeighbour(pos, currentDirection);
+					
+					if(newPos < 0) {
+						// hups da ist ne wand. abbiegen!
+						for(int newDirection = 0; newDirection < 4; newDirection++) {
+							newPos = game.getNeighbour(pos, newDirection);
+							if(newPos != pos && newPos >= 0) {
+								// ausweg gefunden
+								System.out.println("biege ab: " + currentDirection + " -> " + newDirection);
+								currentDirection = newDirection;
+								break;
+							}
+						}
+					}
+					
+					if(game.isJunction(newPos))
+						break;
+					
+					pos = newPos;
+				}
+				// pos is junction
+
+				// kürzesten pfad von geist zu junction finden
+				int minGhostDist = Integer.MAX_VALUE;
+				for(int ghostId = 0; ghostId < G.NUM_GHOSTS; ghostId++) {
+					if(game.getLairTime(ghostId) > 0) continue;
+					
+					int dist = game.getGhostPathDistance(ghostId, pos);
+					if(dist < minGhostDist)
+						minGhostDist = dist;
+				}
+				
+				int minPacManDist = game.getPath(RuleFunctions.currentLocation, pos).length;
+				
+				moveRecommendation.fitness[startDirection] = (float)minGhostDist / (float)minPacManDist * fitness;
+			}
+			/*
 			// TODO junctions berücksichtigen die nicht aufm weg zu einem geist liegen
 			for (Path path : RuleFunctions.getAllGhostPaths().getIterable()) {
 				for(int posInPath = 1; posInPath < path.length() / 2; posInPath++) {
@@ -52,7 +102,7 @@ public class MoveAction {
 					}
 				}
 			}
-			
+			*/
 			return moveRecommendation;
 			
 		default:
