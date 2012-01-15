4c9e0399afbc05116ede8df0707fd98b9d6ff3da
hamena314@freenet.de <hamena314@freenet.de@1dcf074c-340a-367e-bcdd-14ebffc5a148>
2012-01-15 09:01:48 +0000

    In Code reinlesen und Todos verstehen ^^

--- trunk/PacMan/src/game/player/pacman/lcs/EdibleCondition.java
+++ trunk/PacMan/src/game/player/pacman/lcs/EdibleCondition.java
@@ -5,6 +5,7 @@ import game.core.Game;
 /**
  * TODO Wenn 2 Geister in der nähe sind würde aktuell Edible true zurückgeben wenn der eine essbar ist... kann böse enden
  */
+// Also alle Geister durchgehen und wenn in einer bestimmten Distanz z.b. 2 Geister sind, einer essbar der andere nicht, dann flüchten
 public class EdibleCondition implements Condition {
 	
 	final boolean edible;
